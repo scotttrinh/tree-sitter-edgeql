c314a4da6aac910be503343419cb2d29e647221d
Scott Trinh <scott@ca.la>
2023-03-16 09:14:39 -0400

    Add basic node@19 flake

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "Tree-sitter parser for EdgeQL query language for the EdgeDB database";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          devShell = pkgs.mkShell {
+            buildInputs = with pkgs;
+              [
+                nodejs-19_x
+              ];
+          };
+        });
+}
